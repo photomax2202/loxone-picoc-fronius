a7e9c54324644b13bd27bc91e6b949ed5579c294
photomax2202 <maximilian.steinert@me.com>
2025-10-08 16:55:24 +0200

    Update battery_control_functions.c

--- battery_control_functions.c
+++ battery_control_functions.c
@@ -1,29 +1,34 @@
-// INPUT
-// I1 - Entladeschutz Niedertarif	>	Entladen Sperren
-// I2 - Entladeschutz Wallbox		>	Entladen mit Definierter Leistung I9 Eigenverbrauch
-// I3 - Ausgleichsladung				>	Laden mit Definierter Leistung I8 PV Produktion oder 500 W wenn I8 unter 100 W
-// I4 - StorCtl_Mod INPUT
-// I5 - InWRte INPUT
-// I6 - OutWRte INPUT
-// I8 - PV Produktion
-// I9 - Eigenverbrauch
-// I10 - Stromtarif_NT
-// I11 - WChaMax
+/*
+INPUT
+I1 - Entladeschutz Niedertarif	>	Entladen Sperren
+I2 - Entladeschutz Wallbox		>	Entladen mit definierter Leistung I9 Eigenverbrauch
+I3 - Ausgleichsladung				>	Laden mit definierter Leistung I8 PV Produktion - 100 W oder 500 W bei NT
+I4 - StorCtl_Mod INPUT
+I5 - InWRte INPUT
+I6 - OutWRte INPUT
+I8 - PV Produktion
+I9 - Eigenverbrauch
+I10 - Stromtarif_NT
+I11 - WChaMax
+I12 - SOC
 //
-// O1 - StorCtl_Mod
-// O2 - InWRte
-// O3 - OutWRte
+O1 - StorCtl_Mod
+O2 - InWRte
+O3 - OutWRte
 //
-// Modi
-// Entlden Sperren (O1=2, O3=0%)
-// Laden Erzwingen mit definierter Leistung (O1 =3,O3/-O2=Val)
-// Entladen Erzwingen mit definierter Leistung (O1 =3,O2/-O3=Val)
+Modi
+Entlden Sperren (O1=2, O3=0%)
+Laden Erzwingen mit definierter Leistung (O1 =3,O3/-O2=Val)
+Entladen Erzwingen mit definierter Leistung (O1 =3,O2/-O3=Val)
+*/
 
 //Kostanten
 #define BUFF_SIZE 256
-char szBuffer[BUFF_SIZE];
-int localStorCtl_Mod;
-float localInWRte, localOutWRte;
+#define SOC_TH_RECHARGE_LOW 9.5
+#define SOC_TH_RECHARGE_HIGH 10
+char gBuffer[BUFF_SIZE];
+int gBattRecharge;
+float gBattSoc;
 // Funktionen
 float getChargeValuePos(float aValue)
 {
@@ -42,7 +47,7 @@ float getChargeValueNeg(float aValue)
 	return getChargeValuePos(aValue) * -1;
 }
 
-void resetChargeControl(int i)
+void resetChargeControl()
 {
 	setoutput(0,0);
 	setoutput(1,0);
@@ -50,7 +55,7 @@ void resetChargeControl(int i)
 	setoutputtext(0,"Keine Ladesteuerung");
 }
 
-void setBlockUncharge(int i)
+void setBlockUncharge()
 {
 	setoutput(0,2);
 	setoutput(1,0);
@@ -61,58 +66,55 @@ void setBlockUncharge(int i)
 void setChargeValue(float aValue)
 {
 	setoutput(0,3);
-	setoutput(1,getChargeValuePos(aValue));
-	setoutput(2,getChargeValueNeg(aValue));
-	szBuffer = "";
-	sprintf(szBuffer,"Laden mit definierter Leistung: %f W",aValue);
-	setoutputtext(0,szBuffer);
+	setoutput(1,getChargeValueNeg(aValue));
+	setoutput(2,getChargeValuePos(aValue));
+	gBuffer = "";
+	sprintf(gBuffer,"Laden mit definierter Leistung: %f W",aValue);
+	setoutputtext(0,gBuffer);
 }
 
 void setDischargeValue(float aValue)
 {
 	setoutput(0,3);
-	setoutput(1,getChargeValueNeg(aValue));
-	setoutput(2,getChargeValuePos(aValue));
-	szBuffer = "";
-	sprintf(szBuffer,"Entladen mit definierter Leistung: %f W",aValue);
-	setoutputtext(0,szBuffer);
+	setoutput(1,getChargeValuePos(aValue));
+	setoutput(2,getChargeValueNeg(aValue));
+	gBuffer = "";
+	sprintf(gBuffer,"Entladen mit definierter Leistung: %f W",aValue);
+	setoutputtext(0,gBuffer);
 }
 // Main Program
 while(TRUE)
 {
-int lActive;
-lActive = getinput(12);
-//if (lActive > 0)
-//{
 	float lUnchargeProtectNt, lUnchargeProtectWb, lDifferenceCharge, lOwnUsage, lPvProduction;
+	int lEnergyNt;
 	lUnchargeProtectNt = getinput(0);
 	lUnchargeProtectWb = getinput(1);
 	lDifferenceCharge = getinput(2);
-	lOwnUsage = getinput(8)*1000;
-	lPvProduction = getinput(7)*1000;
-
-	if (lUnchargeProtectNt > 0)
+	lEnergyNt = getinput(9);
+	lOwnUsage = getinput(8)*1000; // factor 1000 for conversion kw to w
+	lPvProduction = getinput(7)*1000; // factor 1000 for conversion kw to w
+	
+	gBattSoc = getinput(11);
+	
+	// Check Battery low Soc Recharging
+	if (gBattSoc < SOC_TH_RECHARGE_LOW)
 	{
-		setBlockUncharge(0);
-	}	else {
-		if (lUnchargeProtectWb > 0)
-		{
-			setDischargeValue(lOwnUsage);
-		} else {
-			if (lDifferenceCharge > 0)
-			{
-				if (lPvProduction > 100)
-				{
-					setChargeValue(lPvProduction-100);
-				} else {
-					setChargeValue(500);
-				}	
-			} else {
-				resetChargeControl(0);
-			}
-		}
+		gBattRecharge = 1;
+	} else if (gBattSoc > SOC_TH_RECHARGE_HIGH)
+	{
+		gBattRecharge = 0;
 	}
-//} else {
-//	resetChargeControl(0);
-//}
+
+	// Choose Charging or Blocking mode by inputs
+	if (lUnchargeProtectNt > 0)	{
+		setBlockUncharge();
+	} else if (lUnchargeProtectWb > 0) {
+		setDischargeValue(lOwnUsage);
+	} else if ((lDifferenceCharge > 0) & (lPvProduction > 100)) {
+		setChargeValue(lPvProduction - 100);
+	} else if ((lDifferenceCharge > 0) & (lEnergyNt > 0)) {
+		setChargeValue(500);
+	} else {
+		resetChargeControl();
+	}
 }
